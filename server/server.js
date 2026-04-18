383e02a8dc1a2148e867b60dfdfbe1303d29e0e6
Priyanshu <guptapriyanshu9412@gmail.com>
2026-04-18 22:24:33 +0530

    chng

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "https://sankalan-2026.vercel.app",
+    "https://sankalan.ducss.tech",
     "http://localhost:5173",
     "http://localhost:3000",
   ],
